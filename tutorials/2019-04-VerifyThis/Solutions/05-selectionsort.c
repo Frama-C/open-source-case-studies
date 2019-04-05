03db9a7d4aa0798924fb337cdbce3e4a30dacaf5
Virgile Prevosto <virgile.prevosto@m4x.org>
2019-04-05 14:37:41 +0200

    [VerifyThis] Better naming for selection sort ACSL annotations

--- tutorials/2019-04-VerifyThis/Solutions/05-selectionsort.c
+++ tutorials/2019-04-VerifyThis/Solutions/05-selectionsort.c
@@ -1,26 +1,26 @@
 /*@ predicate sorted{L}(int* a, integer length) =
-      \forall integer i,j; 0<=i<=j<length ==> a[i]<=a[j]; 
+      \forall integer i,j; 0<=i<=j<length ==> a[i]<=a[j];
 */
 /*@ predicate swap{L1,L2}(int* a,integer i,integer j,integer length)=
-      0<=i<j<length 
+      0<=i<j<length
       && \at(a[i],L1) == \at(a[j],L2)
       && \at(a[i],L2) == \at(a[j],L1)
       && \forall integer k; 0<=k<length && k!=i && k!=j ==>
-           \at(a[k],L1) == \at(a[k],L2); 
+           \at(a[k],L1) == \at(a[k],L2);
 */
 /*@ inductive same_elements{L1,L2}(int*a , integer length) {
-      case refl{L}: 
+      case refl{L}:
         \forall int*a, integer length; same_elements{L,L}(a,length);
       case swap{L1,L2}: \forall int*a, integer i,j,length;
         swap{L1,L2}(a,i,j,length) ==> same_elements{L1,L2}(a,length);
       case trans{L1,L2,L3}: \forall int*a, integer length;
         same_elements{L1,L2}(a,length)
-	==> same_elements{L2,L3}(a,length)
-	==> same_elements{L1,L3}(a,length);
+        ==> same_elements{L2,L3}(a,length)
+        ==> same_elements{L1,L3}(a,length);
 }
 */
-/*@ requires \valid(a+(0..length-1));
-    requires length > 0;
+/*@ requires valid_array: \valid(a+(0..length-1));
+    requires non_empty: length > 0;
     assigns a[0..length-1];
     behavior sorted:
       ensures sorted(a,length);
@@ -29,10 +29,10 @@
 */
 void sort (int* a, int length) {
   int current;
-  /*@ loop invariant 0<=current<length;
+  /*@ loop invariant outer_bounds: 0<=current<length;
       loop assigns a[0..length-1],current;
-      for sorted: loop invariant sorted(a,current);
-      for sorted: loop invariant
+      for sorted: loop invariant begin_sort: sorted(a,current);
+      for sorted: loop invariant tail_bigger:
         \forall integer i,j; 0<=i<current<=j<length ==> a[i] <= a[j];
       for same_elements: loop invariant
         same_elements{Pre,Here}(a,length);
@@ -41,11 +41,11 @@ void sort (int* a, int length) {
   for (current = 0; current < length - 1; current++) {
     int min_idx = current;
     int min = a[current];
-    /*@ loop invariant current+1<=i<=length;
+    /*@ loop invariant inner_bounds: current+1<=i<=length;
         loop assigns i,min,min_idx;
-        loop invariant current<=min_idx<i;
-        loop invariant a[min_idx] == min;
-        for sorted: loop invariant
+        loop invariant min_idx_bounds: current<=min_idx<i;
+        loop invariant min_idx_val: a[min_idx] == min;
+        for sorted: loop invariant min_idx_min:
           \forall integer j; current<=j<i ==> min <= a[j];
         loop variant length -i;
     */
@@ -58,7 +58,9 @@ void sort (int* a, int length) {
     if(min_idx != current) {
        L: a[min_idx]=a[current];
        a[current]=min;
-/*@for same_elements:assert swap{L,Here}(a,current,min_idx,length);*/
+       /*@ for same_elements: assert swap:
+             swap{L,Here}(a,current,min_idx,length);
+       */
     }
   }
 }
